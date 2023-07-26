0e6b6b95c565ad134c51abd6794a1cc71810d0f0
Ye Htut Aung <yehtutaung.yha@gmail.com>
2023-07-26 21:48:28 +0800

    Initial commit

---
+++ src/main/java/com/yha/springbootscrudthymeleaf/SpringBootsCrudThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.yha.springbootscrudthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootsCrudThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootsCrudThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yha/springbootscrudthymeleaf/SpringBootsCrudThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yha.springbootscrudthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootsCrudThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
